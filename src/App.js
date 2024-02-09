278b09bcb16b0afe35b28a4f1a4e4a5cb7985aca
Adrian San Segundo <adrian@orvium.io>
2024-02-10 00:09:27 +0100

    chore: init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
