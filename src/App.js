514260ee5d2beb88f598c3688400caeea28c1fc3
Adrian San Segundo <adrian@orvium.io>
2024-02-24 14:25:19 +0100

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer/Footer";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <Login />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import Toolbar from "../components/Toolbar/Toolbar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Toolbar />
+    </div>
+  );
+};
+
+export default Dashboard;
